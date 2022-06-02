7951d03d5f141b16a98566894a78b7818a8953ba
mannbmx <mannbmx@gmail.com>
2022-06-01 21:57:41 -0400

    Started basic layout

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-const contentCont = document.getElementById('content');
+import './style.css';
+
+// const contentCont = document.getElementById('content');
 
 
-const titleDiv = document.createElement('div');
-titleDiv.classList.add('title');
-titleDiv.textContent = "Pappa's Pizzas";
-contentCont.appendChild(titleDiv);
+// const titleDiv = document.createElement('div');
+// titleDiv.classList.add('title');
+// titleDiv.textContent = "Pappa's Pizzas";
+// contentCont.appendChild(titleDiv);
 
-const tabDiv = document.createElement('div');
-tabDiv.classList.add('tabCont');
-contentCont.appendChild(tabDiv);
+// const tabDiv = document.createElement('div');
+// tabDiv.classList.add('tabCont');
+// contentCont.appendChild(tabDiv);
 
-const homeTab = document.createElement('button');
-tabDiv.appendChild(homeTab);
-const menuTab = document.createElement('button');
-tabDiv.appendChild(menuTab);
-const contact = document.createElement('button');
-tabDiv.appendChild(contact);
+// const homeTab = document.createElement('button');
+// tabDiv.appendChild(homeTab);
+// const menuTab = document.createElement('button');
+// tabDiv.appendChild(menuTab);
+// const contact = document.createElement('button');
+// tabDiv.appendChild(contact);
